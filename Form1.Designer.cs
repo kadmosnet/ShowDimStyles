da47c0d9ee8d5e52254539288a72cc1366e85494
Daniele Alberti <kadmos@kadmos.com>
2022-03-24 12:05:50 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ShowDimStyles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DXFReaderNET.DxfDocument dxfDocument1 = new DXFReaderNET.DxfDocument();
+            DXFReaderNET.AciColor aciColor1 = new DXFReaderNET.AciColor();
+            DXFReaderNET.Objects.RasterVariables rasterVariables1 = new DXFReaderNET.Objects.RasterVariables();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dxfReaderNETControl1 = new DXFReaderNET.DXFReaderNETControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dxfReaderNETControl1
+            // 
+            this.dxfReaderNETControl1.AntiAlias = true;
+            this.dxfReaderNETControl1.AutoSize = true;
+            this.dxfReaderNETControl1.ContinuousHighlight = false;
+            this.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None;
+            this.dxfReaderNETControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            dxfDocument1.ActiveLayout = "Model";
+            dxfDocument1.Comments = null;
+            aciColor1.Index = ((short)(256));
+            aciColor1.IsByBlock = false;
+            aciColor1.IsByLayer = true;
+            aciColor1.UseTrueColor = false;
+            dxfDocument1.CurrentColor = aciColor1;
+            dxfDocument1.CurrentElevation = 0D;
+            dxfDocument1.CurrentLayer = "0";
+            dxfDocument1.CurrentLineTypeName = "ByLayer";
+            dxfDocument1.CurrentLineTypeScale = 1D;
+            dxfDocument1.CurrentTextSize = 2.5D;
+            dxfDocument1.CurrentTextStyle = "STANDARD";
+            dxfDocument1.CurrentThickness = 0D;
+            dxfDocument1.DateCreated = new System.DateTime(2022, 3, 23, 10, 11, 59, 350);
+            dxfDocument1.DateLastEdited = new System.DateTime(2022, 3, 23, 10, 11, 59, 350);
+            dxfDocument1.Handle = "0";
+            dxfDocument1.IsBinary = false;
+            dxfDocument1.Modified = false;
+            dxfDocument1.Name = null;
+            dxfDocument1.Owner = null;
+            rasterVariables1.DisplayFrame = true;
+            rasterVariables1.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
+            rasterVariables1.Handle = "2D";
+            rasterVariables1.Owner = null;
+            rasterVariables1.Units = DXFReaderNET.ImageUnits.Unitless;
+            dxfDocument1.RasterVariables = rasterVariables1;
+            dxfDocument1.ThumbnailImage = null;
+            dxfDocument1.TotalEditTime = new System.DateTime(2022, 3, 23, 0, 0, 0, 0);
+            dxfDocument1.TotalEditTimeDays = 0;
+            dxfDocument1.UserTimer = new System.DateTime(2022, 3, 23, 0, 0, 0, 0);
+            dxfDocument1.UserTimerDays = 0;
+            this.dxfReaderNETControl1.DXF = dxfDocument1;
+            this.dxfReaderNETControl1.FileName = null;
+            this.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta;
+            this.dxfReaderNETControl1.HighlightNotContinuous = false;
+            this.dxfReaderNETControl1.Location = new System.Drawing.Point(0, 0);
+            this.dxfReaderNETControl1.Name = "dxfReaderNETControl1";
+            this.dxfReaderNETControl1.ShowAxes = false;
+            this.dxfReaderNETControl1.ShowGrid = true;
+            this.dxfReaderNETControl1.ShowGridRuler = false;
+            this.dxfReaderNETControl1.Size = new System.Drawing.Size(1079, 654);
+            this.dxfReaderNETControl1.TabIndex = 0;
+            this.dxfReaderNETControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dxfReaderNETControl1_MouseDown);
+            this.dxfReaderNETControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dxfReaderNETControl1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(954, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New drawing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(954, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add dimension";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(954, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Dimension styles";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 630);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1079, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 654);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dxfReaderNETControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DXFReader.NET Component - Dimensions Sample ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DXFReaderNET.DXFReaderNETControl dxfReaderNETControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DXFReaderNET;
+using DXFReaderNET.Entities;
+namespace ShowDimStyles
+{
+    public partial class Form1 : Form
+    {
+        internal enum FunctionsEnum
+        {
+            None,
+            Dimension,
+            DimensionLine,
+            DimensionCircle,
+            DimensionArc,
+        }
+        private FunctionsEnum CurrentFunction = FunctionsEnum.None;
+
+        private Line SelectedLine = null;
+        private Circle SelectedCircle = null;
+        private Arc SelectedArc = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Parent = dxfReaderNETControl1;
+            label1.BackColor = System.Drawing.Color.Transparent;
+            label1.Text = "";
+            newDrawing();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newDrawing();
+        }
+
+        private void newDrawing()
+        {
+            dxfReaderNETControl1.NewDrawing();
+            dxfReaderNETControl1.CursorSelectionSize = 8;
+            int k;
+            Random rnd = new Random();
+
+            dxfReaderNETControl1.NewDrawing();
+            dxfReaderNETControl1.SetLimits(new Vector2(-1, -1), new Vector2(-25, 125));
+            dxfReaderNETControl1.ZoomLimits();
+            int n = 3;
+            for (k = 0; k < n; k++)
+            {
+                Vector3 pStart = new Vector3(rnd.NextDouble() * 100, rnd.NextDouble() * 100, 0);
+                Vector3 pEnd = new Vector3(rnd.NextDouble() * 100, rnd.NextDouble() * 100, 0);
+                dxfReaderNETControl1.AddLine(pStart, pEnd, (short)rnd.Next(1, 7));
+
+            }
+
+            for (k = 0; k < n; k++)
+            {
+                Vector3 c = new Vector3(rnd.NextDouble() * 100, rnd.NextDouble() * 100, 0);
+                double r = rnd.NextDouble() * 20;
+                dxfReaderNETControl1.AddCircle(c, r, (short)rnd.Next(1, 7));
+            }
+
+            for (k = 0; k < n; k++)
+            {
+                Vector3 c = new Vector3(rnd.NextDouble() * 100, rnd.NextDouble() * 100, 0);
+                double r = rnd.NextDouble() * 25;
+                dxfReaderNETControl1.AddArc(c, r, rnd.NextDouble() * 360, rnd.NextDouble() * 360, (short)rnd.Next(1, 7));
+            }
+
+
+            dxfReaderNETControl1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHairSquare;
+            CurrentFunction = FunctionsEnum.Dimension;
+            label1.Text = "Select entity with a mouse click";
+        }
+
+        private void SetPoint()
+
+        {
+            EntityObject entity = null;
+            Vector2 p = dxfReaderNETControl1.CurrentWCSpoint;
+            switch (CurrentFunction)
+            {
+
+                case FunctionsEnum.DimensionArc:
+
+                    dxfReaderNETControl1.AddAngularDimension(SelectedArc, p.ToVector3(), dxfReaderNETControl1.DXF.DrawingVariables.DimStyle, dxfReaderNETControl1.DXF.CurrentColor.Index);
+                    Refresh();
+                    label1.Text = "";
+                    CurrentFunction = FunctionsEnum.None;
+                    break;
+                case FunctionsEnum.DimensionCircle:
+
+                    dxfReaderNETControl1.AddRadialDimension(SelectedCircle, p.ToVector3(), dxfReaderNETControl1.DXF.DrawingVariables.DimStyle, dxfReaderNETControl1.DXF.CurrentColor.Index);
+                    Refresh();
+                    label1.Text = "";
+                    CurrentFunction = FunctionsEnum.None;
+                    break;
+                case FunctionsEnum.DimensionLine:
+
+
+                    dxfReaderNETControl1.AddAlignedDimension(SelectedLine, p.ToVector3(), dxfReaderNETControl1.DXF.DrawingVariables.DimStyle, dxfReaderNETControl1.DXF.CurrentColor.Index);
+                    Refresh();
+                    label1.Text = "";
+                    CurrentFunction = FunctionsEnum.None;
+                    break;
+
+                case FunctionsEnum.Dimension:
+
+
+                    dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHair;
+
+                    entity = dxfReaderNETControl1.GetEntity(p);
+                    if (entity != null)
+                    {
+                        switch (entity.Type)
+                        {
+                            case EntityType.Line:
+                                SelectedLine = (Line)entity;
+                                CurrentFunction = FunctionsEnum.DimensionLine;
+                                label1.Text = "Select dimension position";
+                                break;
+                            case EntityType.Circle:
+                                SelectedCircle = (Circle)entity;
+                                CurrentFunction = FunctionsEnum.DimensionCircle;
+                                label1.Text = "Select dimension position";
+                                break;
+                            case EntityType.Arc:
+                                SelectedArc = (Arc)entity;
+                                CurrentFunction = FunctionsEnum.DimensionArc;
+                                label1.Text = "Select dimension position";
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        label1.Text = "No entity found";
+                        dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHair;
+                    }
+                    break;
+            }
+        }
+
+        private void dxfReaderNETControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            SetPoint();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dxfReaderNETControl1.ShowDimStyles();
+        }
+
+        private void dxfReaderNETControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Vector2 p = dxfReaderNETControl1.CurrentWCSpoint;
+            switch (CurrentFunction)
+            {
+                case FunctionsEnum.DimensionLine:
+                    dxfReaderNETControl1.ShowRubberBandAlignedDimension(SelectedLine, p);
+                    break;
+                case FunctionsEnum.DimensionCircle:
+
+                    dxfReaderNETControl1.ShowRubberBandLine(SelectedCircle.Center.ToVector2(), p);
+                    break;
+
+
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            button1.Left = this.Width - button1.Width - 28;
+            button2.Left = button1.Left;
+            button3.Left = button1.Left;
+            label1.Top = this.Height - label1.Height;
+
+        }
+    }
+}
